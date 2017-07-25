a497a7cac0292887b38c81d8a9363eaaff120f05
BlackGad <ztBlackGad@gmail.com>
2017-07-26 00:42:39 +0300

    - Misspell fix

--- PS.Build/Services/IArtifactory.cs
+++ PS.Build/Services/IArtifactory.cs
@@ -10,10 +10,10 @@ namespace PS.Build.Services
         #region Members
 
         /// <summary>
-        ///     Declare artifact MSBuild Item.
+        ///     Declare additional artifact MSBuild Item that will be included to build process before compilation.
         /// </summary>
         /// <param name="path">Path to file</param>
-        /// <param name="type">MSBuil item type</param>
+        /// <param name="type">MSBuild item type</param>
         /// <returns>Fluent artifact item builder</returns>
         IArtifactBuilder Artifact(string path, BuildItem type);
 
