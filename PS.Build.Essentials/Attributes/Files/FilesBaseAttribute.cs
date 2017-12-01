c0a968c5c2a5d9a30584149f20849fa5ae58c2df
Vladimir Shkolka <ztBlackGad@gmail.com>
2017-12-01 17:54:59 +0200

    Essential attributes were reworked Implemented Copy/Move/Remove attributes

---
+++ PS.Build.Essentials/Attributes/Files/FilesBaseAttribute.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using PS.Build.Extensions;
+using PS.Build.Services;
+using PS.Build.Types;
+
+namespace PS.Build.Essentials.Attributes
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public abstract class FilesBaseAttribute : Attribute
+    {
+        #region Constructors
+
+        protected FilesBaseAttribute(string selectPattern, string[] filterPatterns)
+        {
+            SelectPattern = selectPattern;
+            FilterPatterns = filterPatterns;
+            BuildStep = BuildStep.PostBuild;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        ///     Gets or sets a value indicating whether to start the process.
+        /// </summary>
+        public BuildStep BuildStep { get; set; }
+
+        public string[] FilterPatterns { get; }
+
+        public string SelectPattern { get; }
+
+        #endregion
+
+        #region Members
+
+        protected virtual RecursivePath[] EnumerateFiles(IServiceProvider provider, string selectPattern, string[] filterPatterns)
+        {
+            var foundItems = IOExtensions.SearchFiles(selectPattern, (pattern, file) => file.DetermineRecursivePath(pattern))
+                                         .Items
+                                         .ToList();
+
+            var filteredFiles = new List<RecursivePath>();
+            var logger = provider.GetService<ILogger>();
+
+            foreach (var pattern in filterPatterns)
+            {
+                var paths = IOExtensions.Match(foundItems, pattern, path => path.Original).ToList();
+
+                if (paths.Any())
+                {
+                    logger.Info($"Files filtered by {pattern} pattern:");
+                    foreach (var path in paths)
+                    {
+                        logger.Info($"- {path.Original}");
+                    }
+                }
+                else
+                {
+                    logger.Info($"There is no files to filter by {pattern} pattern");
+                }
+
+                filteredFiles.AddRange(paths);
+            }
+
+            filteredFiles = filteredFiles.Distinct().ToList();
+            return foundItems.Except(filteredFiles).ToArray();
+        }
+
+        protected abstract void Process(RecursivePath[] files, IServiceProvider provider);
+
+        private void PostBuild(IServiceProvider provider)
+        {
+            if (BuildStep.HasFlag(BuildStep.PostBuild))
+            {
+                var macroResolver = provider.GetService<IMacroResolver>();
+                var selectPattern = macroResolver.Resolve(SelectPattern);
+                var filterPatterns = FilterPatterns.Enumerate()
+                                                   .Select(f => macroResolver.Resolve(f))
+                                                   .ToArray();
+
+                Process(EnumerateFiles(provider, selectPattern, filterPatterns), provider);
+            }
+        }
+
+        private void PreBuild(IServiceProvider provider)
+        {
+            if (BuildStep.HasFlag(BuildStep.PreBuild))
+            {
+                var macroResolver = provider.GetService<IMacroResolver>();
+                var selectPattern = macroResolver.Resolve(SelectPattern);
+                var filterPatterns = FilterPatterns.Enumerate()
+                                                   .Select(f => macroResolver.Resolve(f))
+                                                   .ToArray();
+                Process(EnumerateFiles(provider, selectPattern, filterPatterns), provider);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ PS.Build/Types/SearchResult.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PS.Build.Types
+{
+    public class SearchResult<T>
+    {
+        #region Constructors
+
+        public SearchResult() : this(null, null)
+        {
+        }
+
+        public SearchResult(string pattern, IEnumerable<T> items)
+        {
+            Items = items ?? Enumerable.Empty<T>();
+            Pattern = pattern ?? string.Empty;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public IEnumerable<T> Items { get; }
+
+        public string Pattern { get; }
+
+        #endregion
+    }
+}
